8646e6ff666241ee5cbb2a5b7d2228797608369d
questionyu <questionyu@outlook.com>
2018-05-26 15:04:21 +0800

    Junit Controller.

--- src/ControllerTest.java
+++ src/ControllerTest.java
@@ -1,74 +1,72 @@
 import junit.framework.TestCase;
 
-public class ControllerTest extends TestCase {
+import java.util.Calendar;
 
+/**
+ * Title        ControllerTest.java
+ * Description  Controller test class.
+ */
+public class ControllerTest extends TestCase {
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
 	public void setUp() throws Exception {
 		super.setUp();
+		ManagerController.startManager();
 	}
 
-	public void testStartManager() {
-	}
-
-	public void testStartGUI() {
-	}
-
+	/**
+	 * Test loadCustomers.
+	 */
 	public void testLoadCustomers() {
+		assertTrue(Controller.loadCustomers());
 	}
 
-	public void testGetMonitors() {
-	}
-
-	public void testGetCustomers() {
-	}
-
-	public void testAddCustomer() {
-	}
-
-	public void testRemoveCustomer() {
-	}
-
-	public void testLogin() {
-	}
-
-	public void testLogout() {
-	}
-
+	/**
+	 * Test getIDType.
+	 */
 	public void testGetIDType() {
+		assertEquals(0, Controller.getIDType(7355608));
+		assertEquals(-1, Controller.getIDType(1234));
+		assertEquals(1, Controller.getIDType(12345678));
 	}
 
-	public void testUpdateTariff() {
+	/**
+	 * Test checkID.
+	 */
+	public void testCheckID() {
+		assertTrue(Controller.checkID(12345678));
+		assertFalse(Controller.checkID(1234));
 	}
 
-	public void testGetBudget() {
+	/**
+	 * Test checkDuplicated.
+	 */
+	public void testCheckDuplicated() {
+		assertTrue(Controller.checkDuplicated(new Customer("Jim", "New York", "jim@example.com", 11223344, 500.0)));
+		assertFalse(Controller.checkDuplicated(new Customer("JimTest", "New York Test", "jim@example.com", 11223344, 500.0)));
 	}
 
-	public void testUpdateBudget() {
+	/**
+	 * Test stringToCalendar.
+	 */
+	public void testStringToCalendar() {
+		String dateString = "2018-6-25";
+		Calendar testDate = Controller.stringToCalendar(dateString);
+		assertEquals(2018, testDate.get(Calendar.YEAR));
+		assertEquals(6, testDate.get(Calendar.MONTH) + 1);
+		assertEquals(25, testDate.get(Calendar.DAY_OF_MONTH));
 	}
 
-	public void testGetReadings() {
-	}
-
-	public void testGetCosts() {
-	}
-
-	public void testGetPriceElectricity() {
-	}
-
-	public void testGetPriceGas() {
-	}
-
-	public void testGetTariffElectricity() {
-	}
-
-	public void testGetTariffGas() {
-	}
-
-	public void testSendReadings() {
-	}
-
-	public void testGetReadingsFromFile() {
-	}
-
-	public void testWriteReadingsToFile() {
+	/**
+	 * Test calendarToString.
+	 */
+	public void testCalendarToString() {
+		Calendar testDate = Calendar.getInstance();
+		testDate.set(Calendar.YEAR, 2018);
+		testDate.set(Calendar.MONTH, 6 - 1);
+		testDate.set(Calendar.DAY_OF_MONTH, 25);
+		assertEquals("2018-6-25", Controller.calendarToString(testDate));
 	}
 }
